4bd93bec83ab0c4123e4c36aef260108115c8d0d
Daniel Millward <dmillward29@gmail.com>
2020-07-25 16:46:31 +0100

    Added premake5 file

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "imgui"
+	language "C++"
+	staticruntime "Off"
+	kind "StaticLib"
+	configurations { "Debug", "Shipping" }
+
+	targetdir (BinaryDirectory)
+	objdir (IntermediateDirectory)
+
+	files
+	{
+		"imgui_draw.cpp"
+		"imgui_internal.h"
+		"imgui_widgets.cpp"
+		"imstb_rectpack.h"
+		"imstb_textedit.h"
+		"imstb_truetype.h"
+		"imconfig.h"
+		"imgui.cpp"
+		"imgui.h"
+		"imgui_demo.cpp"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Shipping"
+		runtime "Release"
+		optimize "On"
+
+	filter "system:windows"
+		systemversion "latest"
+		
